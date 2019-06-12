6e57fc66ab3e974b9b9bec5b1adbad2c28457abc
Huong <huongnguyenak96@gmail.com>
2019-06-12 20:38:40 +0700

    body-parser

--- www/index.js
+++ www/index.js
@@ -1,14 +1,22 @@
 // const express = require('express')
 // const app = express()
 const app = require('express')();
-app.set('view engine','ejs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended:true }))
+app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('home');
 })
-app.get('/signin',(req,res)=>{
-  res.render('signin');
+app.get('/signin', (req, res) => {
+    res.render('signin');
 })
-// app.post('');
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    // const email = req.body.email;
+    // const password = req.body.password;
+    const { email, password }  = req.body;
+    res.send({ email, password });
+});
 
-app.listen(3000, ()=>console.log('Server started on port 3000!'))
+app.listen(3000, () => console.log('Server started on port 3000!'))
