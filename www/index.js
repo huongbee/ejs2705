e64f43d5ad65c9d40356f9220df6b761e747a0e9
Huong <huongnguyenak96@gmail.com>
2019-06-12 20:02:37 +0700

    form

--- www/index.js
+++ www/index.js
@@ -6,5 +6,9 @@ app.set('view engine','ejs');
 app.get('/',(req,res)=>{
     res.render('home');
 })
+app.get('/signin',(req,res)=>{
+  res.render('signin');
+})
+// app.post('');
 
 app.listen(3000, ()=>console.log('Server started on port 3000!'))
