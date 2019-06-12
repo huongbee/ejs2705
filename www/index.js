7f8556049b4690ffac0d2c46c63452b4dc000b68
Huong <huongnguyenak96@gmail.com>
2019-06-12 21:17:57 +0700

    post

--- www/index.js
+++ www/index.js
@@ -2,21 +2,28 @@
 // const app = express()
 const app = require('express')();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended:true }))
+// app.use(bodyParser.urlencoded({ extended:true }))
+app.use(bodyParser.json({
+    type: 'application/json'
+}))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('home');
 })
 app.get('/signin', (req, res) => {
-    res.render('signin');
+    let name = "Teo";
+    let age = "<h3>11</h3>";
+    let arr = [11,12,13,14];
+    res.render('signin', { name: name, txtAge: age , arr});
 })
 app.post('/login', (req, res) => {
     console.log(req.body)
     // const email = req.body.email;
     // const password = req.body.password;
     const { email, password }  = req.body;
-    res.send({ email, password });
+    res.setHeader('token', '1234567');
+    res.send({ name: 'name', email, password });
 });
 
 app.listen(3000, () => console.log('Server started on port 3000!'))
